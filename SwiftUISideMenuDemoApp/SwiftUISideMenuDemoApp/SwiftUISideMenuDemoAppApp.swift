caca948923ccc9c5e1bd2daf879cd1ad4dec5716
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-01-19 02:41:49 +0300

    First Folding

---
+++ SwiftUISideMenuDemoApp/SwiftUISideMenuDemoApp/SwiftUISideMenuDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISideMenuDemoAppApp.swift
+//  SwiftUISideMenuDemoApp
+//
+//  Created by mehmet karanlık on 19.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISideMenuDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
